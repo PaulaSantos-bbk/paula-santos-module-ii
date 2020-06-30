eaa18e7ee751fb72736d0c43a640656522faacfc
unknown <pangeriz@outlook.com>
2020-06-30 18:22:06 +0100

    form

--- src/pages/ContactPage/index.jsx
+++ src/pages/ContactPage/index.jsx
@@ -1,5 +1,47 @@
 import React from "react";
+import "./ContactPage.css";
 
-const ContactPage = () => <div>I am the contact form!</div>;
+const ContactPage = () => {
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+  };
 
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>Contact us</h1>
+      </div>
+      <div className="form">
+        <form>
+          <label htmlFor="fname">*Your name: </label>
+          <br />
+          <input id="name" label="name" type="text" required />
+          <br />
+          <label htmlFor="email">*Your email: </label>
+          <br />
+          <input id="email" label="Your email:" type="email" required />
+          <br />
+          <label htmlFor="comment">*Your Comment: </label>
+          <br />
+          <textarea
+            rows="4"
+            cols="26"
+            name="comment"
+            id="comment"
+            required
+          ></textarea>
+          <br />
+          <button
+            input="submit"
+            type="submit"
+            className="btn"
+            disabled={!onFormSubmit}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 export default ContactPage;
