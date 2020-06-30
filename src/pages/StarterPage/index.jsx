23d1605973730790492074e2312594a80cad611a
unknown <pangeriz@outlook.com>
2020-06-30 20:16:37 +0100

    test

--- src/pages/StarterPage/index.jsx
+++ src/pages/StarterPage/index.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import "./StarterPage.css";
 
-const StarterPage = () => <div>I am the starter!</div>;
+const StarterPage = () => (
+  <div className="container">
+    <div className="hero">
+      <h1 className="h1">I'm the starter!!!</h1>
+    </div>
+    <div className="ingredients">
+      <h2>Ingredients</h2>
+      <p>ingredient one</p>
+      <p>ingredient two</p>
+      <p>ingredient three</p>
+    </div>
+    <div className="method">
+      <h2>Method</h2>
+      <p>step one</p>
+      <p>step two</p>
+      <p>step three</p>
+    </div>
+  </div>
+);
 
 export default StarterPage;
