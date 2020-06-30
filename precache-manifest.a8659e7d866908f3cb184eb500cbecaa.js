5bf179b5e8e2b2298a6cc6104b03a5680b0577ee
unknown <pangeriz@outlook.com>
2020-06-30 18:22:54 +0100

    Updates

---
+++ precache-manifest.a8659e7d866908f3cb184eb500cbecaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3154a02ec5a8007320b57df73688335b",
+    "url": "/paula-santos-module-ii/index.html"
+  },
+  {
+    "revision": "9a4fa1a9694bd238c49a",
+    "url": "/paula-santos-module-ii/static/css/main.1f07c59d.chunk.css"
+  },
+  {
+    "revision": "5fe7392498cc9567a025",
+    "url": "/paula-santos-module-ii/static/js/2.63db604b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/paula-santos-module-ii/static/js/2.63db604b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a4fa1a9694bd238c49a",
+    "url": "/paula-santos-module-ii/static/js/main.05bb7ecc.chunk.js"
+  },
+  {
+    "revision": "58ba730a1f27e1acee2f",
+    "url": "/paula-santos-module-ii/static/js/runtime-main.1ddfc0e7.js"
+  }
+]);
