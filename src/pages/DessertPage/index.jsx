bac4bc4857a39b4acecfeba404da0339b515cbff
unknown <pangeriz@outlook.com>
2020-06-30 20:58:20 +0100

    basic pages

--- src/pages/DessertPage/index.jsx
+++ src/pages/DessertPage/index.jsx
@@ -1,5 +1,23 @@
 import React from "react";
 
-const DessertPage = () => <div>I am the dessert!</div>;
+const DessertPage = () => (
+  <div className="container">
+    <div className="hero">
+      <h1 className="h1">I'm the dessert!!!</h1>
+    </div>
+    <div className="ingredients">
+      <h2>Ingredients</h2>
+      <p>ingredient one</p>
+      <p>ingredient two</p>
+      <p>ingredient three</p>
+    </div>
+    <div className="method">
+      <h2>Method</h2>
+      <p>step one</p>
+      <p>step two</p>
+      <p>step three</p>
+    </div>
+  </div>
+);
 
 export default DessertPage;
--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,5 +1,23 @@
 import React from "react";
 
-const MainPage = () => <div>I am the main course!</div>;
+const MainPage = () => (
+  <div className="container">
+    <div className="hero">
+      <h1 className="h1">I'm the main course!!!</h1>
+    </div>
+    <div className="ingredients">
+      <h2>Ingredients</h2>
+      <p>ingredient one</p>
+      <p>ingredient two</p>
+      <p>ingredient three</p>
+    </div>
+    <div className="method">
+      <h2>Method</h2>
+      <p>step one</p>
+      <p>step two</p>
+      <p>step three</p>
+    </div>
+  </div>
+);
 
 export default MainPage;
